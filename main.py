9bc4b6c796514511982d5c7d5fe65040ecec2150
Chris <chriswaligo@berkeley.edu>
2025-12-01 12:37:50 -0800

    set up runnable main

--- main.py
+++ main.py
@@ -3,17 +3,39 @@ Entry point
 """
 import sys
 from evantrace.parser import Parser
+from evantrace.writer import Writer
+from evantrace.caches import Cache
+from evantrace.sim import Sim
 
 def main():
     print("Hello from evantrace! Beginning Parser test...")
-    if len(sys.argv) < 1:
-        print("No file to be parsed was provided.")
+    if len(sys.argv) < 2:
+        print("Need an input and an output file.")
         
     parser = Parser(sys.argv[1])
-    parser.parse()
+    instructions = parser.parse()
     
+    l2cache = Cache(
+        associativity=4,
+        total_size=524_288,
+        read_latency=12
+    )
     
-
+    icache = Cache(
+        associativity=4,
+        parent=l2cache
+    )
+    
+    dcache = Cache(
+        associativity=4,
+        parent=l2cache
+    )
+    
+    sim = Sim(trace=instructions, icache=icache, dcache=dcache, l2cache=l2cache)
+    sim.run()
+    
+    writer = Writer(sys.argv[2])
+    writer.write(instructions)
 
 if __name__ == "__main__":
     main()
